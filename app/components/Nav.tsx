6e26290967b00cb653d15be56461727de59938fa
Joseph Lozano <git@josephlozano.xyz>
2022-04-02 20:33:42 -0400

    email me

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,22 +1,26 @@
 import { Link } from "@remix-run/react";
 export default function Nav() {
   return (
-    <div className="spacing-x-8 flex h-16 items-center bg-sky-500 pl-8 ">
-      <Link to="/" className="mr-6 h-4/5 items-center">
+    <div className="flex h-16 items-center space-x-8 bg-sky-500 pl-8">
+      <Link to="/" className="h-4/5 items-center">
         <img
           className="h-full rounded-full"
           src="/images/rabbit.jpeg"
           alt="a rabbit riding a snail"
         />
       </Link>
-      <ul className="items-center">
-        <Link
-          to="/blog"
-          className="text-xl font-bold text-slate-100 hover:text-slate-200"
-        >
-          <li>Blog</li>
-        </Link>
-      </ul>
+      <Link
+        to="/blog"
+        className="text-xl font-bold text-slate-100 hover:text-slate-200"
+      >
+        Blog
+      </Link>
+      <a
+        href="mailto:blog@josephlozano.xyz"
+        className="text-xl font-bold text-slate-100 hover:text-slate-200"
+      >
+        Email me
+      </a>
     </div>
   );
 }
